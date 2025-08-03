a742301681a3a647259e23ac48e68f5ff99c39f5
georgeslight <georgeslight@gmail.com>
2025-08-03 14:45:04 +0200

    feuermuehle turnable

--- internal/model/worksModel.go
+++ internal/model/worksModel.go
@@ -36,6 +36,24 @@ func GetWorksData() WorksData {
 				Href:        "/works/die-zwei-jacobs",
 				Description: "A beautiful turnable item",
 			},
+			{
+				ID:       "feuermuehle",
+				Title:    "Feuermühle",
+				Category: "turnables",
+				Images: []WorksImage{
+					{Src: "/static/images/works/turnables/feuermuehle/feuermuehle-nb-01.png", Alt: "Feuermühle 01"},
+					{Src: "/static/images/works/turnables/feuermuehle/feuermuehle-nb-02.png", Alt: "Feuermühle 02"},
+					{Src: "/static/images/works/turnables/feuermuehle/feuermuehle-nb-03.png", Alt: "Feuermühle 03"},
+					{Src: "/static/images/works/turnables/feuermuehle/feuermuehle-nb-04.png", Alt: "Feuermühle 04"},
+					{Src: "/static/images/works/turnables/feuermuehle/feuermuehle-nb-05.png", Alt: "Feuermühle 05"},
+					{Src: "/static/images/works/turnables/feuermuehle/feuermuehle-nb-06.png", Alt: "Feuermühle 06"},
+					{Src: "/static/images/works/turnables/feuermuehle/feuermuehle-nb-07.png", Alt: "Feuermühle 07"},
+					{Src: "/static/images/works/turnables/feuermuehle/feuermuehle-nb-08.png", Alt: "Feuermühle 08"},
+					{Src: "/static/images/works/turnables/feuermuehle/feuermuehle-nb-09.png", Alt: "Feuermühle 09"},
+				},
+				Href:        "/works/feuermuehle",
+				Description: "A beautiful turnable item",
+			},
 		},
 	}
 }
