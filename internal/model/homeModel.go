4f780d130f12842f92b7dafa8017a98fb85833ab
georgeslight <georgeslight@gmail.com>
2025-11-22 19:58:37 +0100

    home cleanup and moved final images

--- internal/model/homeModel.go
+++ internal/model/homeModel.go
@@ -33,46 +33,6 @@ func GetHomeData() HomeData {
 				Src: "/static/images/home/kristin-04.jpg",
 				Alt: "Home Image 04",
 			},
-			{
-				ID:  "home5",
-				Src: "/static/images/home/kristin-05.jpg",
-				Alt: "Home Image 05",
-			},
-			{
-				ID:  "home6",
-				Src: "/static/images/home/kristin-06.jpg",
-				Alt: "Home Image 06",
-			},
-			{
-				ID:  "home7",
-				Src: "/static/images/home/kristin-07.jpg",
-				Alt: "Home Image 07",
-			},
-			{
-				ID:  "home8",
-				Src: "/static/images/home/kristin-08.jpg",
-				Alt: "Home Image 08",
-			},
-			{
-				ID:  "home09",
-				Src: "/static/images/home/kristin-test-01.jpg",
-				Alt: "Home Image 09",
-			},
-			{
-				ID:  "home10",
-				Src: "/static/images/home/kristin-test-02.jpg",
-				Alt: "Home Image 10",
-			},
-			// {
-			// 	ID:  "home12",
-			// 	Src: "/static/images/home/b012.jpg",
-			// 	Alt: "Home Image 12",
-			// },
-			// {
-			// 	ID:  "home13",
-			// 	Src: "/static/images/home/b013.jpg",
-			// 	Alt: "Home Image 13",
-			// },
 		},
 	}
 }
