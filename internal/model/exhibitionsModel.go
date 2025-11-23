00ad8c5d1d509c5020dc25ab2f785ea6c870fb2f
georgeslight <georgeslight@gmail.com>
2025-11-23 18:40:21 +0100

    new test exhibiton group entry ith new pics

--- internal/model/exhibitionsModel.go
+++ internal/model/exhibitionsModel.go
@@ -165,22 +165,30 @@ func GetExhibitionsData() ExhibitionsData {
 				Date:     "2023",
 				Href:     "/exhibitions/group-exhibition-example",
 				BaseImage: ExhibitionImage{
-					Src: "/static/images/exhibitions/moma/moma-01.jpg",
+					Src: "/static/images/exhibitions/group-01/new-01.jpg",
 					Alt: "Test Image 01",
 				},
 				Details: ExhibitionDetails{
 					Images: []ExhibitionImage{
 						{
-							Src: "/static/images/exhibitions/moma/moma-01.jpg",
+							Src: "/static/images/exhibitions/group-01/new-01.jpg",
 							Alt: "Test Image 01",
 						},
 						{
-							Src: "/static/images/exhibitions/moma/moma-02.jpg",
+							Src: "/static/images/exhibitions/group-01/new-02.jpg",
 							Alt: "Test Image 02",
 						},
 						{
-							Src: "/static/images/exhibitions/moma/moma-03.jpg",
+							Src: "/static/images/exhibitions/group-01/new-03.jpg",
 							Alt: "Test Image 03",
+						},
+						{
+							Src: "/static/images/exhibitions/group-01/new-04.jpg",
+							Alt: "Test Image 04",
+						},
+						{
+							Src: "/static/images/exhibitions/group-01/new-05.jpg",
+							Alt: "Test Image 05",
 						},
 					},
 					Description: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.\n\nDuis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.\n\nUt wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.",
